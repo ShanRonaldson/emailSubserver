fa4e770206fb391af2c9f406904c1d061ecb0376
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-08-21 10:22:45 +0300

    no message

--- app.js
+++ app.js
@@ -8,22 +8,20 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function(req, res){
-    res.sendFile(`${__dirname}/signup.html`);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
-    
-    var firstName = req.body.fName;
-    var surname = req.body.sName;
-    var email = req.body.email;
-    
-    console.log(firstName, surname, email);
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var surname = req.body.sName;
+  var email = req.body.email;
+
+  console.log(firstName);
 });
 
-
-app.listen(3000, function(){
-    console.log("Server is running on port 3000.");
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
 });
